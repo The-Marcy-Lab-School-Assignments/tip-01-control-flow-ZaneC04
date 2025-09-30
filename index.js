a15f73529d4673658d41686578024105bc3f4e97
ZaneC04 <zenithzsc@gmail.com>
2025-09-30 10:50:10 -0400

    modified index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 /*
+
 Complete the function so that it returns the following:
 80 or above → "Wear shorts and a t-shirt."
 60–79 → "Wear jeans and a light jacket."
@@ -7,7 +8,16 @@ Below 40 → "Wear a heavy coat, scarf, and boots."
 */
 
 const outfitPicker = (temp) => {
-  // ✍️ Write your code here
+  if (temp >= 80) {
+    return "Wear shorts and a t-shirt."
+  } else if (temp >= 60 && temp <= 79) {
+    return "Wear jeans and a light jacket."
+  } else if (temp >= 40 && temp <= 59) {
+    return "Wear a sweater and pants."
+  } else {
+    return "Wear a heavy coat, scarf, and boots."
+  };
 };
 
 //Test your function below
+console.log(outfitPicker(40))
